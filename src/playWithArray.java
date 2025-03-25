50383a6d4bc7c430e2cf00fe6cc51e0db4ab00c6
hideo aoyama <100831251+boukendesho@users.noreply.github.com>
2025-03-25 21:47:19 +0900

    Play with Array.

---
+++ src/playWithArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class playWithArray {
+    public static void main(String[] args) {
+        // An Array with items
+        String[] GPUBrands = {"NVIDIA", "AMD", "INTEL"};
+        boolean noMatched = true;
+
+        // User input
+        String choice;
+        System.out.print("Which brand of GPU you want?: ");
+        Scanner scanner = new Scanner(System.in);
+        choice = scanner.nextLine().toUpperCase();
+
+        for (String gpu : GPUBrands) {
+            // Check if user's input is matching items in Array
+            if (choice.equals(gpu)) {
+                System.out.printf("Ahh here is your %s GPU\n", gpu);
+                noMatched = false;
+                break;
+            }
+        }
+
+        if (noMatched) {
+            System.out.println("Can't find your favorite GPU Brand.");
+        }
+    }
+}
