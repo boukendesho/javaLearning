d775a958bcbbeb5060d6a6f8eba3157aaf413b7b
hideo aoyama <100831251+boukendesho@users.noreply.github.com>
2025-03-27 00:24:08 +0900

    Replace katakana with emoji. Add computer score record.

--- src/janKenPon.java
+++ src/janKenPon.java
@@ -6,16 +6,17 @@ public class janKenPon {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        String[] actions = {"グー", "パー", "チョキ"};
+        String[] actions = {"✊", "\uD83E\uDD1A", "✌️"};
         int choice;
         int computerMove;
         boolean gameContinue = true;
-        int score = 0;
+        int youWin = 0;
+        int computerWin = 0;
         String isContinued;
 
         System.out.println("じゃんけんぽん");
         do {
-            System.out.print("1.グー 2.パー 3.チョキ　から一つ選んで :");
+            System.out.print("1.✊ 2.\uD83E\uDD1A 3.✌️　から一つ選んで :");
             choice = scanner.nextInt() - 1;
             scanner.nextLine();
 
@@ -30,12 +31,13 @@ public class janKenPon {
 
             if ((choice == 0 && computerMove == 2) || (choice == 1 && computerMove == 0) || (choice == 2 && computerMove == 1)) {
                 System.out.println("あなたの勝利");
-                score += 1;
+                youWin += 1;
             } else {
                 System.out.println("パソコンの勝利");
+                computerWin += 1;
             }
 
-            if (score > 0) {
+            if (youWin > 0) {
                 System.out.print("続く? (Y/N): ");
                 isContinued = scanner.nextLine().toUpperCase();
 
@@ -43,9 +45,11 @@ public class janKenPon {
                     continue;
                 } else {
                     System.out.println("いい勝負だったよ！");
-                    System.out.printf("あなたの点数: %d\n", score);
+                    System.out.printf("あなたの得点: %d　パソコンの得点: %d\n", youWin, computerWin);
                 }
 
+            } else {
+                System.out.println("人間ってこんなのレベルか・・・");
             }
 
             gameContinue = false;
