4cb8d9327986848fc00c19b3a782d90514186d08
hideo aoyama <100831251+boukendesho@users.noreply.github.com>
2025-03-26 23:46:09 +0900

    Add game score record and game continue function.

--- src/janKenPon.java
+++ src/janKenPon.java
@@ -10,11 +10,14 @@ public class janKenPon {
         int choice;
         int computerMove;
         boolean gameContinue = true;
+        int score = 0;
+        String isContinued;
 
         System.out.println("じゃんけんぽん");
         do {
             System.out.print("1.グー 2.パー 3.チョキ　から一つ選んで :");
             choice = scanner.nextInt() - 1;
+            scanner.nextLine();
 
             computerMove = random.nextInt(actions.length);
 
@@ -27,11 +30,26 @@ public class janKenPon {
 
             if ((choice == 0 && computerMove == 2) || (choice == 1 && computerMove == 0) || (choice == 2 && computerMove == 1)) {
                 System.out.println("あなたの勝利");
+                score += 1;
             } else {
                 System.out.println("パソコンの勝利");
             }
 
+            if (score > 0) {
+                System.out.print("続く? (Y/N): ");
+                isContinued = scanner.nextLine().toUpperCase();
+
+                if (isContinued.equals("Y")) {
+                    continue;
+                } else {
+                    System.out.println("いい勝負だったよ！");
+                    System.out.printf("あなたの点数: %d\n", score);
+                }
+
+            }
+
             gameContinue = false;
+
 
         } while (gameContinue);
 
